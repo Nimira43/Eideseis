bce0424006a09d8101e994f9b5b9162a90b3ce08
Nimira43 <n.rabone72@aol.com>
2024-12-11 04:29:47 +0000

    Set up category nav-links

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -21,7 +21,21 @@ const NewsPage = () => {
             <img src={userImage} alt="Lucy Blog" />
             <p>Lucy's Blog</p>
           </div>
-          <nav className='categories'>Categories</nav>
+          <nav className='categories'>
+            <h1 className='nav-heading'>Categories</h1>
+            <div className="nav-links">
+              <a href="#" className="nav-link">UK & Ireland</a>
+              <a href="#" className="nav-link">Europe</a>
+              <a href="#" className="nav-link">World</a>
+              <a href="#" className="nav-link">Sports</a>
+              <a href="#" className="nav-link">Entertainment</a>
+              <a href="#" className="nav-link">Health</a>
+              <a href="#" className="nav-link">Business</a>
+              <a href="#" className="nav-link">Technology</a>
+              <a href="#" className="nav-link">Science</a>
+              <a href="#" className="nav-link">Arts</a>
+            </div>
+          </nav>
         </div>
         <div className='news-section'>
             <div className='headline'>Headline</div>
